0ef53019307f333f3881d055b4610c224ada46c3
Jacob <jeden@supremainc.com>
2025-05-13 16:25:40 +0100

    Add project files.

---
+++ Serial to MAC/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Serial_to_MAC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainLogo = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.mainLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialNum
+            // 
+            this.serialNum.Location = new System.Drawing.Point(49, 132);
+            this.serialNum.Name = "serialNum";
+            this.serialNum.Size = new System.Drawing.Size(183, 20);
+            this.serialNum.TabIndex = 0;
+            this.serialNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Serial Number";
+            // 
+            // mainLogo
+            // 
+            this.mainLogo.Image = ((System.Drawing.Image)(resources.GetObject("mainLogo.Image")));
+            this.mainLogo.Location = new System.Drawing.Point(12, 47);
+            this.mainLogo.Name = "mainLogo";
+            this.mainLogo.Size = new System.Drawing.Size(291, 66);
+            this.mainLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mainLogo.TabIndex = 2;
+            this.mainLogo.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(28, 170);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(265, 166);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "MAC Address";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Serial Number";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 384);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.mainLogo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serialNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.mainLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox serialNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox mainLogo;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
