de2cdf753d84934ca0c610d595f67017fdc65cc8
Jacob Eden <jeden@supremainc.com>
2025-05-26 15:02:15 +0100

    updates

--- Serial to MAC/Form1.Designer.cs
+++ Serial to MAC/Form1.Designer.cs
@@ -28,19 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.serialNum = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.mainLogo = new System.Windows.Forms.PictureBox();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.calculate = new System.Windows.Forms.Button();
+            this.copyMac = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.copySerial = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.mainLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
             // serialNum
             // 
-            this.serialNum.Location = new System.Drawing.Point(49, 132);
+            this.serialNum.Location = new System.Drawing.Point(109, 121);
             this.serialNum.Name = "serialNum";
             this.serialNum.Size = new System.Drawing.Size(183, 20);
             this.serialNum.TabIndex = 0;
@@ -49,7 +55,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(102, 116);
+            this.label1.Location = new System.Drawing.Point(166, 105);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 1;
@@ -58,7 +64,7 @@
             // mainLogo
             // 
             this.mainLogo.Image = ((System.Drawing.Image)(resources.GetObject("mainLogo.Image")));
-            this.mainLogo.Location = new System.Drawing.Point(12, 47);
+            this.mainLogo.Location = new System.Drawing.Point(66, 26);
             this.mainLogo.Name = "mainLogo";
             this.mainLogo.Size = new System.Drawing.Size(291, 66);
             this.mainLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -68,37 +74,76 @@
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2});
+            this.columnHeader3,
+            this.columnHeader4});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(28, 170);
+            this.listView1.Location = new System.Drawing.Point(66, 196);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(265, 166);
+            this.listView1.Size = new System.Drawing.Size(265, 176);
             this.listView1.TabIndex = 3;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
-            // columnHeader1
+            // columnHeader3
             // 
-            this.columnHeader1.Text = "MAC Address";
+            this.columnHeader3.Text = "Serial Number";
+            this.columnHeader3.Width = 135;
             // 
-            // columnHeader2
+            // columnHeader4
             // 
-            this.columnHeader2.Text = "Serial Number";
-            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Text = "MAC Address";
+            this.columnHeader4.Width = 135;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(164, 147);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 4;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // copyMac
+            // 
+            this.copyMac.Location = new System.Drawing.Point(35, 147);
+            this.copyMac.Name = "copyMac";
+            this.copyMac.Size = new System.Drawing.Size(103, 23);
+            this.copyMac.TabIndex = 5;
+            this.copyMac.Text = "Copy All MAC Address";
+            this.copyMac.UseVisualStyleBackColor = true;
+            this.copyMac.Click += new System.EventHandler(this.copyMac_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // copySerial
+            // 
+            this.copySerial.Location = new System.Drawing.Point(267, 147);
+            this.copySerial.Name = "copySerial";
+            this.copySerial.Size = new System.Drawing.Size(103, 23);
+            this.copySerial.TabIndex = 6;
+            this.copySerial.Text = "Copy All Serial Numbers";
+            this.copySerial.UseVisualStyleBackColor = true;
+            this.copySerial.Click += new System.EventHandler(this.copySerial_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(410, 384);
+            this.Controls.Add(this.copySerial);
+            this.Controls.Add(this.copyMac);
+            this.Controls.Add(this.calculate);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.mainLogo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.serialNum);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Serial To Mac Address";
             ((System.ComponentModel.ISupportInitialize)(this.mainLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,8 +155,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox mainLogo;
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ColumnHeader columnHeader1;
-        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button copyMac;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button copySerial;
     }
 }
 
--- Serial to MAC/Form1.cs
+++ Serial to MAC/Form1.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 
 namespace Serial_to_MAC
 {
@@ -17,9 +20,65 @@ namespace Serial_to_MAC
             InitializeComponent();
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private void calculate_Click(object sender, EventArgs e)
         {
+            string num = serialNum.Text;
 
+            
+
+                if (num.Length == 9)
+                {
+                    int hexVal = Convert.ToInt32(num);
+
+                    string newHexVal = hexVal.ToString("X");
+
+                    string subString = newHexVal.Substring(2);
+                    string formatted = $"00:17:FC:{subString.Substring(0, 2)}:{subString.Substring(2, 2)}:{subString.Substring(4, 2)}";
+
+
+                    listView1.Items.Add(new ListViewItem(new string[] { num, formatted }));
+                    serialNum.Clear();
+                }
+                else
+                {
+                    
+                    MessageBox.Show("Serial Number Must be 9 Digits", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                }
+                
+
+                
+            
+            
+
+        }
+
+        private void copyMac_Click(object sender, EventArgs e)
+        {
+            string clipText = string.Empty;
+            foreach (ListViewItem item in listView1.Items)
+            {
+                clipText += item.SubItems[1].Text;
+                clipText += Environment.NewLine;
+            }
+            if (!String.IsNullOrEmpty(clipText))
+            {
+                Clipboard.SetText(clipText);
+            }
+        }
+
+        private void copySerial_Click(object sender, EventArgs e)
+        {
+            string clipText = string.Empty;
+            foreach (ListViewItem item in listView1.Items)
+            {
+                clipText += item.SubItems[0].Text;
+                clipText += Environment.NewLine;
+            }
+            if (!String.IsNullOrEmpty(clipText))
+            {
+                Clipboard.SetText(clipText);
+            }
         }
     }
 }
